0296326988da7826ca7229c79ee7bf828aa17974
vincdeprinz <vincentstucki9@gmail.com>
2024-05-22 09:57:29 +0200

    CSS und Grundgerüst von Website 1

--- src/main/java/ch/bbw/ff/film/FilmController.java
+++ src/main/java/ch/bbw/ff/film/FilmController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FilmController{
@@ -11,10 +13,17 @@ public class FilmController{
     @Autowired
     private FilmService filmService;
 
+    @GetMapping("/index")
+    public String filmSuchen(Model model){
+        model.addAttribute("filmSuchen",filmService.getFilmList());
+        return"index";
+    }
     @GetMapping("/listeFilm")
     public String showFilm(Model model){
         model.addAttribute("filmlist",filmService.getFilmList());
         return"listeFilm";
     }
+
+    
 }
 
